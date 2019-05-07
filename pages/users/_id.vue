6dfbb4a6637c24005232307b81a2f2aca5bb0bf8
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-05-06 23:06:00 -0700

    Validating Parameters (to allow certain params in url)

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -6,8 +6,20 @@
 
 <script>
     export default {
-        name: "_id"
+      name: "_id",
+
+      // NUXT-SPECIFIC feature "validate"  to make sure only valid dynamic url segments reach a specific url
+      // validate should return true
+      validate(data){
+        //  1) A page will only load if the id is "1", otherwise, page is not found
+        // return data.params.id == 1
+
+        // 2)  A page will load only load if id is a number
+        return /^\d+$/.test(data.params.id) //will  return true if id is a number
+      }
     }
+
+
 </script>
 
 <style scoped>
