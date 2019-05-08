5756f700d27f135a87adcad6e24e35b1b57ec5ec
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-05-07 22:23:42 -0700

    Add Error Page.

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>An Error accured, we are sorry!!!!</h1>
+</template>
+<!--Just create an error.vue file and it will automatically pop up in case of error. No need to inject it anywhere.-->
+<script>
+    export default {
+        name: "error"
+    }
+</script>
+
+<style scoped>
+
+</style>
